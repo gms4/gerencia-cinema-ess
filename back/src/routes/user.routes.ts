1dfdede8615370d1ac9bc97537dc9cfb5c0e9752
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-09 22:38:59 -0300

    Atualizando pré-criado User
    Atualizando model (back e front), modificando funcoes de criacao de
    usuarios e removendo funcoes que nao serao utilizadas e adicionando
    regras de negocio.

--- back/src/routes/user.routes.ts
+++ back/src/routes/user.routes.ts
@@ -17,15 +17,20 @@ userRouter.route("/")
     .post((req: Request, res: Response) => {
         
         let name = req.body.name;
-        let age = parseInt(req.body.age);
+        let cpf = req.body.cpf;
+        let telefone = req.body.telefone;
         let email = req.body.email;
-        const newUser = userController.createUser(name, age, email);
+        let endereco = req.body.endereco;
+        let senha = req.body.senha;
+        let cargo = req.body.cargo;
+        const resultCreate = userController.createUser(name, cpf, telefone, email, endereco, senha, cargo);
 
-        if (newUser){
-            return res.json({ message: "Usuário criado com sucesso!"});
-        }
-
-        return res.status(409).json({ error: "Email já cadastrado!"});
+        if (resultCreate == "emailError"){
+            return res.status(409).json({ error: "Email já cadastrado!"});
+        } else if (resultCreate == "cpfError") {
+            return res.status(409).json({ error: "CPF já cadastrado!"});
+        } // if (worked correctly)
+        return res.json({ message: cargo + " cadastrado com sucesso!"});
     })
     .put((req: Request, res: Response) => {
         return res.json({ Warning: "Método PUT não suportado para a rota /users"});
@@ -33,7 +38,7 @@ userRouter.route("/")
     .delete((req: Request, res: Response) => {
         return res.json({ Warning: "Método DELETE não suportado para a rota /users"});
     });
-
+/*
 userRouter.route("/:id")
     .get((req: Request, res: Response) => {
 
@@ -45,9 +50,7 @@ userRouter.route("/:id")
 
         return res.json({ user });
     })
-    .post((req: Request, res: Response) => {
-        return res.json({ Warning: "Método POST não suportado para a rota /users/:id" });
-    })
+    
     .put((req: Request, res: Response) => {
         let id = parseInt(req.params.id);
         let name = req.body.name;
@@ -59,17 +62,6 @@ userRouter.route("/:id")
         } 
 
         return res.status(200).json({ success: "Usuário atualizado com sucesso." });
-    })
-    .delete ((req: Request, res: Response) => {
-        let id = parseInt(req.params.id);
-
-        let deleteUser = userController.deleteUser(id);
-
-        if (!deleteUser) {
-            return res.status(404).json({ error: "Usuário não encontrado." });
-        } 
-
-        return res.status(200).json({ success: "Usuário deletado com sucesso." });
-    });
+    })*/
     
 export default userRouter;
