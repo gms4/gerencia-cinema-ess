92636697663f7e852bbf72b633193cd1723738df
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-14 17:38:31 -0300

    Adicionando funcao de login ao front
    Adicionando novo serviço e conectando suas funções a tela de login e ao
    server

--- front/src/app/tela-login/tela-login.component.ts
+++ front/src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+
 
 @Component({
   selector: 'app-tela-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaLoginComponent implements OnInit {
 
-  constructor() { }
+  emailOrCpf: string;
+  password: string;
+
+  constructor(private AuthenticationService: AuthenticationService) { 
+    this.emailOrCpf = "";
+    this.password = "";
+  }
 
   ngOnInit(): void {
   }
 
+  login(): void{
+    if (this.emailOrCpf == "") {
+      alert("Por favor, digite o seu CPF ou Email");
+    }else if (this.password == "") {
+      alert("Por favor, digite a sua senha");
+    }else{
+      this.AuthenticationService.login(this.emailOrCpf, this.password).subscribe({
+        next: (user) => {
+          this.emailOrCpf = "";
+          this.password = "";
+          this.AuthenticationService.setUserLogado(user);
+          //navegar para a tela de filmes
+        },
+        error: (err) => {
+          alert (err.error.error);
+        }
+      })
+    }
+  }
 }
