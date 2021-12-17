60ac2419b3e92315e61c18c18e3a8e08f527b823
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-17 11:48:51 -0300

    Melhorando visualização da tela filmes
    Adicionando visualização do tempo em horas/minutos e tambem mostrando todas as sessões do filme

--- front/src/app/tela-filmes/tela-filmes.component.ts
+++ front/src/app/tela-filmes/tela-filmes.component.ts
@@ -2,6 +2,7 @@ import { TelaAreaFuncionarioComponent } from './../tela-area-funcionario/tela-ar
 import { Filme } from 'src/app/models/filme';
 import { FilmesService } from '../services/filmes.service';
 import { Component, OnInit } from '@angular/core';
+import { sessaoFilme } from '../models/sessaoFilme';
 
 @Component({
   selector: 'app-tela-filmes',
@@ -27,5 +28,36 @@ export class TelaFilmesComponent implements OnInit {
         alert("Não foi possível obter os filmes do servidor");
       }
     })
+  }
+
+  duracaoMinHours(qtdTotalMin: number): string{
+    let qtdHours = Math.floor (qtdTotalMin/60);
+    let qtdHoursMin : number = qtdTotalMin % 60;
+
+    if (qtdHours == 0){
+      return qtdTotalMin + "min";
+    }
+    if (qtdHoursMin == 0) {
+      return qtdTotalMin + "min (" + qtdHours + "h)";  
+    }
+    if (qtdHoursMin < 10) {
+      return qtdTotalMin + "min (" + qtdHours + "h0" + qtdHoursMin + "min)";  
+    }
+    return qtdTotalMin + "min (" + qtdHours + "h" + qtdHoursMin + "min)";  
+  }
+
+  extractHorarios(sessoes: sessaoFilme[]): string{
+    let horarios : string = "";
+    let iterations : number = 0;
+
+    for(let sessao of sessoes){
+      if (iterations == sessoes.length - 1) {
+        horarios += sessao.hora;
+      }else{
+        horarios += sessao.hora + ", ";
+      }
+      iterations++;
+    }
+    return horarios;
   }
 }
