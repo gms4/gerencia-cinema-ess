8dbd136b089b8aa80ef7a4cc2e18b7bcf94e572e
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-19 17:56:39 -0300

    Adicionando guia das rotas
    Adicionando guia para o desenvolvimento das rotas de filmes e
    sessoesSala

--- back/src/routes/filmes.routes.ts
+++ back/src/routes/filmes.routes.ts
@@ -12,9 +12,25 @@ filmesRouter.route("/")
         return res.json(filmes);
     })
     .post((req: Request, res: Response) => {
+        /*
+            Deve verificar se o filme está com todos os atributos necessarios
+            para ser cadastrado
+            Deve cadastrar o filme enviado no body no filmesController,
+            suas sessoes devem ser cadastradas na sala especificada, 
+            OBS1: Seus dois atributos devem ser deduzidos nesse post
+            OBS2: Filmes arquivados não podem cadastrar sessoes e nem ter
+            atributos deduzidos
+        */
         return res.json({ Warning: "Ainda será desenvolvido"});
     })
     .put((req: Request, res: Response) => {
+        /*
+            Deve modificar APENAS filmes arquivados e deve verificar se tem
+            todos os atributos necessarios para ser cadastrado
+            OBS1: Caso o filme tenha atributos suficientes para deixar de 
+            ser arquivado, seus atributos devem ser deduzidos aqui e suas
+            sessoes devem ser cadastradas no sessoesSala
+        */
         return res.json({ Warning: "Ainda será desenvolvido"});
     });
     
--- back/src/routes/sessoesSala.routes.ts
+++ back/src/routes/sessoesSala.routes.ts
@@ -8,20 +8,39 @@ const sessoesSalaController =  new SessoesSalaController();
 
 sessoesSalaRouter.route("/:numeroSala")
     .get((req: Request, res: Response) => {
+        /*
+            Deve retornar todas as sessoes da sala especificada (req.params.numeroSala)
+        */
         return res.json ({Warning: "Essa rota ainda sera desenvolvida"});
     })
     .post((req: Request, res: Response) => {
+        /*
+            Deve criar nova sessao em uma determinada sala (req.params.numeroSala), 
+            a sessao nova sera recebida pelo req.body
+        */
         return res.json({Warning: "Essa rota ainda sera desenvolvida"});
     });
 
 sessoesSalaRouter.route("/:numeroSala/:horario")
     .get((req: Request, res: Response) => {
+    	/*
+            Deve retornar a sessao especificada da sala especificada
+            (req.params.numeroSala e req.params.horario)
+        */
         return res.json ({warning: "Essa rota ainda sera desenvolvida"});
     })
     .post((req: Request, res: Response) => { 
+        /*
+            Deve retornar a sessao especificada da sala especificada
+            (req.params.numeroSala e req.params.horario)
+        */ 
         return res.json({warning: "Essa rota ainda sera desenvolvida"});
     })
     .put((req: Request, res: Response) => {
+        /*
+            Deve modificar a qtd de ingressos e o array de cadeiras da sessaoSala
+            (req.params.numeroSala e req.params.horario)
+        */
         return res.json({ Warning: "Essa rota ainda sera desenvolvida"});
     });
     
