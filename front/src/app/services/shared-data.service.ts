a5e23d4bf64ae83f0064a82a328f2bf2d4ab995d
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-18 23:48:04 -0300

    Adicionando serviço de dados compartilhados
    Adicionando serviço shared-data para posterior uso

---
+++ front/src/app/services/shared-data.service.ts
@@ -0,0 +1,41 @@
+import { sessaoFilme } from './../models/sessaoFilme';
+import { Filme } from 'src/app/models/filme';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedDataService {
+
+  private filmeSelecionado: Filme | null;
+  private sessaoFilmeSelecionada: sessaoFilme | null;
+
+  constructor() { 
+    this.filmeSelecionado = null;
+    this.sessaoFilmeSelecionada = null;
+  }
+
+  getFilmeSelecionado(): Filme | null{
+    return this.filmeSelecionado;
+  }
+  
+  getsessaoFilmeSelecionada(): sessaoFilme | null{
+    return this.sessaoFilmeSelecionada;
+  }
+  
+  setFilmeSelecionado(filmeSelecionado: Filme){
+    this.filmeSelecionado = filmeSelecionado;
+  }
+
+  setsessaoFilmeSelecionada(sessaoFilmeSelecionada: sessaoFilme){
+    this.sessaoFilmeSelecionada = sessaoFilmeSelecionada;
+  }
+
+  removeFilmeSelecionado(){
+    this.filmeSelecionado = null;
+  }
+
+  removeSalaSelecionada(){
+    this.sessaoFilmeSelecionada = null
+  }
+}
