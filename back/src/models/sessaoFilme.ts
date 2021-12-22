c6afdcc049b2ea90cb8453147fe5d579d6b326ad
RenattoPadilha <rspf@cin.ufpe.br>
2021-12-21 21:14:57 -0300

    Atualizando models
    Atualizando models para modelo melhor idealizado para funcionamento da
    tela-filmes

--- back/src/models/sessaoFilme.ts
+++ back/src/models/sessaoFilme.ts
@@ -3,11 +3,13 @@ export class sessaoFilme{
     //Feito para que nao fosse necessaria a duplicacao de informacao
 
     sala: string;
+    tipo: string;
     hora: string;
     lotacao: string;
 
-    constructor (sala: string, hora: string, lotacao: string){
+    constructor (sala: string, hora: string, tipo: string, lotacao: string){
         this.sala = sala;
+        this.tipo = tipo;
         this.hora = hora;
         this.lotacao = lotacao;
     }
